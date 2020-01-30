653a9afe5c2546fa9dd104e44b70f6bd47828871
jonas.cavalcanti-goldentechnolog <jonas.cavalcanti@goldentechnologia.com.br>
2020-01-30 15:39:21 -0300

    Set hierar

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/jonascavalcantineto/cloud-connector/connectors/aws"
 	"github.com/google/go-cmp/cmp"
+	"github.com/jonascavalcantineto/cloud-connector/connectors"
 )
 
 func main() {
 	fmt.Println(connectors.AWSApiStart())
-	 fmt.Println(connectors.AzureApiStart())
+	fmt.Println(connectors.AzureApiStart())
 	// fmt.Println(connectors.GCPApiStart())
 	// fmt.Println(connectors.ReverseRunes("jonascavalcantineto"))
 	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
